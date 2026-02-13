58a2e18a89b806d2fe4db570582f2507ec42fd1c
Ihsane Abdou <ihsane@certicar.pro>
2026-02-13 16:39:41 +0000

    File changes

--- src/components/ui/slider.jsx
+++ src/components/ui/slider.jsx
@@ -1,3 +1,4 @@
+
 import * as React from "react"
 import * as SliderPrimitive from "@radix-ui/react-slider"
 
@@ -9,11 +10,11 @@ const Slider = React.forwardRef(({ className, ...props }, ref) => (
     className={cn("relative flex w-full touch-none select-none items-center", className)}
     {...props}>
     <SliderPrimitive.Track
-      className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-primary/20">
-      <SliderPrimitive.Range className="absolute h-full bg-primary" />
+      className="relative h-1.5 w-full grow overflow-hidden rounded-full bg-[#1C64F2]/20">
+      <SliderPrimitive.Range className="absolute h-full bg-[#1C64F2]" />
     </SliderPrimitive.Track>
     <SliderPrimitive.Thumb
-      className="block h-4 w-4 rounded-full border border-primary/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50" />
+      className="block h-4 w-4 rounded-full border border-[#1C64F2]/50 bg-background shadow transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-[#1C64F2] disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
